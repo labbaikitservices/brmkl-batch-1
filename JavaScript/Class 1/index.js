37e5546a46fdd831421024750247bd8faec82fd4
ZahoorOnly <i.am.zahoor.a@outlook.com>
2024-05-13 23:02:18 +0500

    Class 1 JavaScipt

---
+++ JavaScript/Class 1/index.js
@@ -0,0 +1,16 @@
+// console.log("Hello World!");
+// console.log("Good Bye World!");
+// console.log("Hello my World of Programming!");
+// console.log("Good bye my old World");
+
+// alert("Are you sure?");
+// prompt("Enter a Number");
+
+// let x = prompt("Enter a Number");
+// let y = " Value";
+// z = x + y;
+
+let z = 9;
+z = z + 1;
+
+console.log(z);
