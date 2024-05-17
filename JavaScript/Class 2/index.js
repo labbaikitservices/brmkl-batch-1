aa7bb2a64be81aeb2c5d23d6ca6ce36b821f8f36
ZahoorOnly <i.am.zahoor.a@outlook.com>
2024-05-17 22:20:56 +0500

    feat: javascript class 2

---
+++ JavaScript/Class 2/index.js
@@ -0,0 +1,38 @@
+// let a = "5";
+
+// console.log("Type of a is: ", typeof a);
+// let b = parseInt(a);
+// console.log("Type of b is: ", typeof b);
+
+// console.log("a", a, a, a, a, a);
+
+// Javascript has 8 types
+
+// 1. String
+// 2. Number
+// 3. Bigint
+// 4. Boolean
+// 5. Undefined
+// 6. Null
+// 7. Symbol
+// 8. Object
+
+// 2. An array
+// 3. A date
+
+let x = 5;
+let y = 10;
+
+// Arithmetic Operations:
+
+let addition = x + y;
+let subtraction = x - y;
+let multplication = x * y;
+let division = x / y;
+let mod = x % y;
+
+console.log("Addition: ", addition);
+console.log("subtraction: ", subtraction);
+console.log("multplication: ", multplication);
+console.log("division: ", division);
+console.log("mod: ", mod);
