85379e84da040f54ea6110516ae051703b6abd95
ZahoorOnly <i.am.zahoor.a@outlook.com>
2024-05-19 21:29:59 +0500

    feat: ifs and Loops

---
+++ JavaScript/Class 3/index.js
@@ -0,0 +1,32 @@
+// const age = parseInt(prompt("Enter you age:"));
+// const ageNumber = parseInt(age);
+
+// DRY = Don't Repeat Yourself
+
+// const condition = age >=18;
+// console.log(condition);
+
+// if (age >= 18) {
+//   console.log("You can apply");
+// }
+// else {
+//     console.log("You cannot apply")
+// }
+
+if (18 > 19) {
+  console.log("Hello");
+  console.log("Hello");
+  console.log("Hello");
+  console.log("Hello");
+  console.log("Hello");
+} else {
+  console.log("first");
+}
+
+// counter = counter + 1;
+// counter++;
+
+for (let counter = 0; counter <= 5; counter++) {
+  // console.log()
+  console.log("Sorry", counter);
+}
